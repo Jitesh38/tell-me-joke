993ef88174905b6d63bf615a947dda7277bd9be7
Jitesh38 <jp2112003@gmail.com>
2023-06-08 12:40:33 +0530

    first commit

---
+++ jokes.js
@@ -0,0 +1,361 @@
+let joke=[ 
+`
+Q: What goes up and down but does not move?\n
+A: Stairs
+`,
+`Q: Where should a 500 pound alien go?\n
+A: On a diet
+`,
+`Q: What did one toilet say to the other?\n
+A: You look a bit flushed.
+`,`
+Q: Why did the picture go to jail?\n
+A: Because it was framed.
+`,`
+Q: What did one wall say to the other wall?\n
+A: I'll meet you at the corner.
+`,
+`Q: What did the paper say to the pencil?\n
+A: Write on!
+`,`
+Q: What do you call a boy named Lee that no one talks to?\n
+A: Lonely
+`,`
+Q: What gets wetter the more it dries?\n
+A: A towel.
+`,`
+Q: Why do bicycles fall over?\n
+A: Because they are two-tired!
+`,`
+Q: Why do dragons sleep during the day?\n
+A: So they can fight knights!
+`,`
+Q: What did Cinderella say when her photos did not show up?\n
+A: Someday my prints will come!
+`,`
+Q: Why was the broom late?\n
+A: It over swept!
+`,`
+Q: What part of the car is the laziest?\n
+A: The wheels, because they are always tired!
+`,`
+Q: What did the stamp say to the envelope?\n
+A: Stick with me and we will go places!
+`,
+`Q: What is blue and goes ding dong?\n
+A: An Avon lady at the North Pole!
+`,
+`Q: Were you long in the hospital?\n
+A: No, I was the same size I am now!
+`,
+`Q: Why couldn't the pirate play cards?\n
+A: Because he was sitting on the deck!
+`,
+`Q: What did the laundryman say to the impatient customer?\n
+A: Keep your shirt on!
+`,
+`Q: What's the difference between a TV and a newspaper?\n
+A: Ever tried swatting a fly with a TV?\n
+`,
+`Q: What did one elevator say to the other elevator?\n
+A: I think I'm coming down with something!
+`,
+`Q: Why was the belt arrested?\n
+A: Because it held up some pants!
+`,
+`Q:, Why was everyone so tired on April 1st?\n
+A: They had just finished a March of 31 days.
+`,
+`Q: Which hand is it better to write with?\n
+A: Neither, it's best to write with a pen!
+`,
+`
+Q: Why can't your nose be 12 inches long?\n
+A: Because then it would be a foot!
+`,
+`
+Q: What makes the calendar seem so popular?\n
+A: Because it has a lot of dates!
+`,
+`Q: Why did Mickey Mouse take a trip into space?\n
+A: He wanted to find Pluto!
+`,
+`Q: What is green and has yellow wheels?\n
+A: Grass…..I lied about the wheels!
+`,
+`Q: What is it that even the most careful person overlooks?\n
+A: Her nose!
+`,
+`Q: Did you hear about the robbery last night?\n
+A: Two clothes pins held up a pair of pants!
+`,
+`Q: Why do you go to bed every night?\n
+A:, Because the bed won't come to you!
+`,
+`Q: Why did Billy go out with a prune?\n
+A: Because he couldn't find a date!
+`,
+`Q: Why do eskimos do their laundry in Tide?\n\n
+A: Because it's too cold out-tide!
+`,
+`Q: How do you cure a headache?\n\n
+A: Put your head through a window and the pane will just disappear!
+`,
+`Q: What has four wheels and flies?\n\n
+A: A garbage truck!
+`,
+`Q: What kind of car does Mickey Mouse's wife drive?\n\n
+A: A minnie van!
+`,
+`Q: Why don't traffic lights ever go swimming?\n \n
+A: Because they take too long to change!
+`,
+`Q: Why did the man run around his bed?\n\n
+A: To catch up on his sleep!
+`,
+`Q: Why did the robber take a bath before he stole from the bank?\n\n
+A: He wanted to make a clean get away!
+`,
+`
+"Why don't scientists trust atoms?\n" Because they make up everything.
+`,`
+"I was wondering why the ball kept getting bigger," and then it hit me.
+`,`
+"I used to be a baker, but I couldn't make enough dough."
+`,`
+"What do you call a fake noodle?\n" An impasta.
+`,`
+"I'm reading a book about anti-gravity." It's impossible to put down!
+`,`
+"What do you call a snowman with a six-pack?\n" An abdominal snowman.
+`,`
+"Why don't skeletons fight each other?\n" They don't have the guts.
+`,`
+"Did you hear about the mathematician who's afraid of negative numbers?\n" He'll stop at nothing to avoid them.
+`,`
+"I used to play piano by ear," but now I use my hands.
+`,`
+"Why did the scarecrow win an award?\n" Because he was outstanding in his field.
+`,`
+"How do you organize a space party?\n" You planet.
+`,`
+"Why don't vampires go to barbecues?\n" They don't like steak.
+`,`
+"What do you call a bear with no teeth?\n" A gummy bear.
+`,`
+"I used to be addicted to the hokey pokey." But then I turned myself around.
+`,`
+"Why did the bicycle fall over?\n" It was two-tired.
+`,`
+"What did the grape say when it got stepped on?\n" Nothing. It just let out a little wine.
+`,`
+"Why don't skeletons fight each other?\n" They don't have the guts.
+`,`
+"Why couldn't the bicycle stand up by itself?\n" It was two-tired.
+`,`
+"Why did the tomato turn red?\n" Because it saw the salad dressing.
+`,`
+"Why did the golfer bring two pairs of pants?\n" In case he got a hole in one.
+`,`
+"Why don't scientists trust atoms?\n" Because they make up everything.
+`,`
+"What do you call a fake noodle?\n" An impasta.
+`,`
+"I'm reading a book about anti-gravity." It's impossible to put down!
+`,`
+"Why don't skeletons fight each other?\n" They don't have the guts.
+`,`
+"Did you hear about the mathematician who's afraid of negative numbers?\n" He'll stop at nothing to avoid them.
+`,`
+"I used to play piano by ear," but now I use my hands.
+`,`
+"Why did the scarecrow win an award?\n" Because he was outstanding in his field.
+`,`
+"How do you organize a space party?\n" You planet.
+`,`
+"Why don't vampires go to barbecues?\n" They don't like steak.
+`,`
+"What do you call a bear with no teeth?\n" A gummy bear.
+`,`
+"I used to be addicted to the hokey pokey." But then I turned myself around.
+`,`
+"Why did the bicycle fall over?\n" It was two-tired.
+`,`
+"What did the grape say when it got stepped on?\n" Nothing. It just let out a little wine.
+`,`
+"Why couldn't the bicycle stand up by itself?\n" It was two-tired.
+`,`
+"Why did the tomato turn red?\n" Because it saw the salad dressing.
+`,`
+"Why did the golfer bring two pairs of pants?\n" In case he got a hole in one.
+`,`
+"Why don't eggs tell jokes?\n" Because they might crack up.
+`,`
+"What did the ocean say to the beach?\n" Nothing, it just waved.
+`,`
+"Why don't skeletons fight each other?\n" They don't have the guts.
+`,`
+"How do you make a tissue dance?\n" You put a little boogie in
+`,`
+"What do you call a fish wearing a crown?\n" King of the sea.
+`,`
+"Why did the scarecrow win an award?\n" Because he was outstanding in his field.
+`,`
+"How do you organize a space party?\n" You planet.
+`,`
+"Why don't vampires go to barbecues?\n" They don't like steak.
+`,`
+"What do you call a bear with no teeth?\n" A gummy bear.
+`,`
+"I used to be addicted to the hokey pokey." But then I turned myself around.
+`,`
+"Why did the bicycle fall over?\n" It was two-tired.
+`,`
+"What did the grape say when it got stepped on?\n" Nothing. It just let out a little wine.
+`,`
+"Why couldn't the bicycle stand up by itself?\n" It was two-tired.
+`,`
+"Why did the tomato turn red?\n" Because it saw the salad dressing.
+`,`
+"Why don't scientists trust atoms?\n" Because they make up everything.
+`,`
+"What do you call a fake noodle?\n" An impasta.
+`,`
+"I'm reading a book about anti-gravity." It's impossible to put down!
+`,`
+"Why don't skeletons fight each other?\n" They don't have the guts.
+`,`
+"Did you hear about the mathematician who's afraid of negative numbers?\n" He'll stop at nothing to avoid them.
+`,`
+"I used to play piano by ear," but now I use my hands.
+`,`
+"Why did the scarecrow win an award?\n" Because he was outstanding in his field.
+`,`
+"How do you organize a space party?\n" You planet.
+`,`
+"Why don't vampires go to barbecues?\n" They don't like steak.
+`,`
+"What do you call a bear with no teeth?\n" A gummy bear.
+`,`
+"I used to be addicted to the hokey pokey." But then I turned myself around.
+`,`
+"Why did the bicycle fall over?\n" It was two-tired.
+`,`
+"What did the grape say when it got stepped on?\n" Nothing. It just let out a little wine.
+`,`
+"Why couldn't the bicycle stand up by itself?\n" It was two-tired.
+`,`
+"Why did the tomato turn red?\n" Because it saw the salad dressing.
+`,`
+"Why did the golfer bring two pairs of pants?\n" In case he got a hole in one.
+`,`
+"Why don't eggs tell jokes?\n" Because they might crack up.
+`,`
+"What did the ocean say to the beach?\n" Nothing, it just waved.
+`,`
+"Why don't skeletons fight each other?\n" They don't have the guts.
+`,`
+"How do you make a tissue dance?\n" You put a little boogie in it.
+`,`
+"What do you call a fish wearing a crown?\n" King of the sea.
+`,`
+"Why did the scarecrow win an award?\n" Because he was outstanding in his field.
+`,`
+"How do you organize a space party?\n" You planet.
+`,`
+"Why don't vampires go to barbecues?\n" They don't like steak.
+`,`
+"What do you call a bear with no teeth?\n" A gummy bear.
+`,`
+"I used to be addicted to the hokey pokey." But then I turned myself around.
+`,`
+"Why did the bicycle fall over?\n" It was two-tired.
+`,`
+"What did the grape say when it got stepped on?\n" Nothing. It just let out a little wine.
+`,`
+"Why couldn't the bicycle stand up by itself?\n" It was two-tired.
+`,`
+"Why did the tomato turn red?\n" Because it saw the salad dressing.
+`,`
+"Why don't scientists trust atoms?\n" Because they make up everything.
+`,`
+"What do you call a fake noodle?\n" An impasta.
+`,`
+"I'm reading a book about anti-gravity." It's impossible to put down!
+`,`
+"Why don't skeletons fight each other?\n" They don't have the guts.
+`,`
+"Did you hear about the mathematician who's afraid of negative numbers?\n" He'll stop at nothing to avoid them.
+`,`
+"I used to play piano by ear," but now I use my hands.
+`,`
+"Why did the scarecrow win an award?\n" Because he was outstanding in his field.
+`,`
+"How do you organize a space party?\n" You planet.
+`,`
+"Why don't vampires go to barbecues?\n" They don't like steak.
+`,`
+"What do you call a bear with no teeth?\n" A gummy bear.
+`,`
+"I used to be addicted to the hokey pokey." But then I turned myself around.
+`,`
+"Why did the bicycle fall over?\n" It was two-tired.
+`,`
+"What did the grape say when it got stepped on?\n" Nothing. It just let out a little wine.
+`,`
+"Why couldn't the bicycle stand up by itself?\n" It was two-tired.
+`,`
+"Why did the tomato turn red?\n" Because it saw the salad dressing.
+`,`
+"Why did the golfer bring two pairs of pants?\n" In case he got a hole in one.
+`,`
+"Why don't eggs tell jokes?\n" Because they might crack up.
+`,`
+"What did the ocean say to the beach?\n" Nothing, it just waved.
+`,`
+"Why don't skeletons fight each other?\n" They don't have the guts.
+`,`
+"How do you make a tissue dance?\n" You put a little boogie in it.
+`,`
+Why don't scientists trust atoms?\n Because they make up everything!
+`,`
+ I asked the librarian if they had any books on paranoia. She whispered, "They're right behind you!"
+`,`
+Why don't skeletons fight each other?\n They don't have the guts!
+`,`
+Why don't scientists trust stairs?\n Because they're always up to something!
+`,`
+I used to play piano by ear, but now I use my hands.
+`,`
+Why was the math book sad?\n Because it had too many problems!
+`,`
+Why did the scarecrow win an award?\n Because he was outstanding in his field!
+`,`
+What do you call a fake noodle?\n An impasta!
+`,`
+ Why don't oysters donate to charity?\n Because they are shellfish!
+`,`
+How do you organize a space party?\n You "planet"!`
+]
+
+console.log(typeof joke)
+
+
+let button = document.getElementById("btn")
+
+button.addEventListener("click",function () {
+    let x = Math.floor(Math.random()*148 + 1)
+let ultimate = joke[x]
+
+console.log(ultimate)
+
+
+
+let box = document.getElementById("box")
+
+console.log(box)
+
+box.innerHTML=`${ultimate}`
+
+})
+
